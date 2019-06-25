249bf8d4cf4403958ed07d4f6af7c8505056b0d4
Shubham Gupta <shubham.gupta@scripbox.com>
2019-06-25 19:03:59 +0000

    EMR clusters via airflow

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,14 +2,9 @@ cd /home/ubuntu/mars
 echo "Resetting mars"
 git fetch
 git checkout -f master
-git reset --hard origin/master
-git clean -df
-git submodule update --recursive --force
-echo "Setting environment variables"
-bash scripts/variables.sh
+git pull origin master
 echo "Restarting services"
 sudo systemctl daemon-reload
 sudo systemctl restart airflow-webserver
 sudo systemctl restart airflow-scheduler
-sudo systemctl restart airflow-worker
 echo "Setup done"
